f234b061bae3892b9862c410e74dda9acc6bedea
Soham jain <sohamjain125@gmail.com>
2025-06-17 15:48:40 +0530

    _app.tsx and query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,3 @@
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { useState } from 'react';
-
-// export default function App({ Component , pageProps }: { Component: React.ComponentType<any>, pageProps: any }) {
-//   const [queryClient] = useState(() => new QueryClient());
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <Component {...pageProps} />
-//     </QueryClientProvider>
-//   );
-// }  
+export default function App({ Component, pageProps }: { Component: React.ComponentType<any>, pageProps: any }) {
+  return <Component {...pageProps} />;
+}  
